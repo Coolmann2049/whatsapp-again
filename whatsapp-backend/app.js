21a258488e89f91c9a975274511827092afa7cc0
Coolmann2049 <coolmann2053@gmail.com>
2025-08-27 20:28:41 +0530

    { limit: '5mb' }

--- whatsapp-backend/app.js
+++ whatsapp-backend/app.js
@@ -37,7 +37,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser()); 
 app.use(compression());
